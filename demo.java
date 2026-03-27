b3c30784f20b90be12420e682ed1d5b371b53291
Prem <bandarupremkishore6@gmail.com>
2026-03-27 06:40:41 +0000

    Added successfully

--- demo.java
+++ demo.java
@@ -4,7 +4,8 @@ git status to --check the status
 git add --to move the changes to the staging area
 git commit -m"Type the message" --to save staged changs to the repository as a snapshot
 git push --sends your committed changes from your local machine to the remote repository
-git diff <filename> --Displays the changes made in files 
+git diff <filename> --Displays the changes made in file
+git show <filename> --Show the code in the file 
 git fetch --used to download the latest changes from a remote repository without merging them into your current branch
 git merge -- used to combine changes from one branch into another branch
 git pull --updates your local project with the latest changes from the remote repository
