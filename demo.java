0530e41f23a53fcf7e9bf9063524f133c1e215d4
Prem <bandarupremkishore6@gmail.com>
2026-03-27 06:35:52 +0000

    This is my demo project

---
+++ demo.java
@@ -0,0 +1,12 @@
+HELLO WORLD!
+
+git status to --check the status
+git add --to move the changes to the staging area
+git commit -m"Type the message" --to save staged changs to the repository as a snapshot
+git push --sends your committed changes from your local machine to the remote repository
+git diff <filename> --Displays the changes made in files 
+git fetch --used to download the latest changes from a remote repository without merging them into your current branch
+git merge -- used to combine changes from one branch into another branch
+git pull --updates your local project with the latest changes from the remote repository
+git branch --shows all local branches
+git checkout --used to switch between branches
